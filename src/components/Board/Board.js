d6290b123b6617b1ba9d879314083eea4b45b27c
Tyler Watson <tylerww91@gmail.com>
2023-01-26 17:59:10 -0800

    initializes board state in GameContext, passes state to App.js for testing purposes, maps through board state to render a box for each object in the array

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,12 +1,17 @@
 import React, { useContext } from 'react';
 import { GameContext } from '../../context/GameContext.js';
+import Box from '../Box/Box.js';
 import './Board.css';
 
 export default function Board() {
   const { board, setBoard } = useContext(GameContext);
   return (
     <main className="main">
-      <div className="board-container">{/* {board.map((box))} */}</div>
+      <div className="board-container">
+        {board.map((box, i) => (
+          <Box key={i} {...{ box }} />
+        ))}
+      </div>
     </main>
   );
 }
--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -4,15 +4,15 @@ const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
   const [board, setBoard] = useState([
-    { space: '', content: '' },
-    { space: '', content: '' },
-    { space: '', content: '' },
-    { space: '', content: '' },
-    { space: '', content: '' },
-    { space: '', content: '' },
-    { space: '', content: '' },
-    { space: '', content: '' },
-    { space: '', content: '' },
+    { space: '0', content: '' },
+    { space: '1', content: '' },
+    { space: '2', content: '' },
+    { space: '3', content: '' },
+    { space: '4', content: '' },
+    { space: '5', content: '' },
+    { space: '6', content: '' },
+    { space: '7', content: '' },
+    { space: '8', content: '' },
   ]);
   const [currentPlayer, setCurrentPlayer] = useState();
   const [active, setActive] = useState();
