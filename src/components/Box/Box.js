3c0d795b20852fd7ff800176e89fbf463c7a88f8
Tyler Watson <tylerww91@gmail.com>
2023-02-14 12:35:16 -0800

    removes last comment

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -3,11 +3,5 @@ import { GameContext } from '../../context/GameContext.js';
 
 export default function Box({ content, space }) {
   const { spaceClick, currentPlayer } = useContext(GameContext);
-  return (
-    <div
-      //if content is set to X / content isn't '' return X otherwise O
-      className={content}
-      onClick={() => spaceClick(currentPlayer, space, content)}
-    ></div>
-  );
+  return <div className={content} onClick={() => spaceClick(currentPlayer, space, content)}></div>;
 }
